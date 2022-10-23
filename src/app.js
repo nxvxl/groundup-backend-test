5c3de6a93312808d224dae868a81982cf2884652
noval <noval.maulana@inphosoft.com>
2022-10-23 14:30:47 +0700

    add cors; include fe build

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 const errorHandler = require("./middlewares/error-handler")
 const router = require("./router")
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
 app.use(express.static("public"))
 app.use('/api/v1/alerts', router)
 app.use(errorHandler)
